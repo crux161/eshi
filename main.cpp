708d565e8c48198f72f1abaaac5a0a35fea3266e
crux161 <eain161@gmail.com>
2025-12-27 19:50:28 -0800

    refactored cpu renderer in prep to add gpu support

--- main.cpp
+++ main.cpp
@@ -1,60 +1,4 @@
-#include "glsl_core.h"
-
-#ifndef LINK_SHADER
-	#include "shader.cpp"
-#else
-	void mainImage(vec4 &fragColor, vec2 fragCoord, vec2 iResolution, float iTime);
-#endif
-
-#include <string>
-#include <omp.h>
-
-std::string get_filename(std::string path) {
-    const size_t last_slash_idx = path.find_last_of("\\/");
-    if (std::string::npos != last_slash_idx) {
-        path.erase(0, last_slash_idx + 1);
-    }
-
-    const size_t period_idx = path.rfind('.');
-    if (std::string::npos != period_idx) {
-        path.erase(period_idx);
-    }
-    return path;
-}
-
-int main(int argc, char **argv) {
-
-    // todo: replace, parse from somewhere else
-    const int W = 16 * 60;
-    const int H = 9 * 60;
-    const int FRAMES = 240;
-    const int FPS = 60;
-
-    std::string output_file = "output.mp4"; // default
-    
-    if (argc > 0) {
-	std::string bin_name = get_filename(argv[0]);
-	if(!bin_name.empty()){
-		output_file = bin_name + ".mp4";
-	}
-    }
-
-    printf("Initializing Encoder: %s\n", output_file.c_str());
-
-    SimpleEncoder video(output_file.c_str(), W, H, FPS);
-
-    #ifdef _OPENMP
-        printf("Using OpenMP with %d cores.\n", omp_get_max_threads());
-    #endif
-
-    for (int i = 0; i < FRAMES; ++i) {
-        float time = (float)i / (float)FRAMES; // 0.0 to 1.0
-
-        int stride;
-        uint8_t* pixels = video.get_pixel_buffer(stride);
-        vec2 iResolution((float)W, (float)H);
-
-       /* Lambda clamping
+/* Lambda clamping
 	*
 	* This version produces slightly off results,
 	* with integer overflow noticable in macOS espcially.
@@ -84,34 +28,57 @@ int main(int argc, char **argv) {
         }
 	*
 	*/
+#include "glsl_core.h"
+#include "renderer_cpu.h" // Include our new worker
+#include <string>
+#include <iostream>
 
-       #pragma omp parallel for
-        for (int y = 0; y < H; ++y) {
-            for (int x = 0; x < W; ++x) {
-                vec4 color;
-                vec2 fragCoord((float)x, (float)y);
-                
-                // Render the pixel
-                mainImage(color, fragCoord, iResolution, time);
+// Helper to extract filename
+std::string get_filename(std::string path) {
+    const size_t last_slash_idx = path.find_last_of("\\/");
+    if (std::string::npos != last_slash_idx) path.erase(0, last_slash_idx + 1);
+    const size_t period_idx = path.rfind('.');
+    if (std::string::npos != period_idx) path.erase(period_idx);
+    return path;
+}
 
-                // Calculate memory index
-                int idx = y * stride + x * 3;
+int main(int argc, char** argv) {
+    // --- CONFIGURATION ---
+    const int W = 16 * 60;   // 960 px
+    const int H = 9 * 60;    // 540 px
+    const int FRAMES = 240;
+    const int FPS = 60;
 
-                // --- ROBUST CLAMPING FIX ---
-                // 1. Clamp float between 0.0 and 1.0
-                // 2. Multiply by 255
-                // 3. Cast to integer
-                
-                float r = fmaxf(0.0f, fminf(color.x, 1.0f));
-                float g = fmaxf(0.0f, fminf(color.y, 1.0f));
-                float b = fmaxf(0.0f, fminf(color.z, 1.0f));
-
-                pixels[idx + 0] = (uint8_t)(r * 255.0f);
-                pixels[idx + 1] = (uint8_t)(g * 255.0f);
-                pixels[idx + 2] = (uint8_t)(b * 255.0f);
-            }
-        }
-	video.submit_frame();
+    // --- SETUP OUTPUT NAME ---
+    std::string output_name = "output.mp4";
+    if (argc > 0) {
+        std::string bin_name = get_filename(argv[0]);
+        if (!bin_name.empty()) output_name = bin_name + ".mp4";
     }
+
+    // --- INITIALIZE SYSTEMS ---
+    printf("Initializing Engine...\n");
+    printf("Output: %s [%dx%d @ %d FPS]\n", output_name.c_str(), W, H, FPS);
+
+    SimpleEncoder video(output_name.c_str(), W, H, FPS);
+    CpuRenderer renderer(W, H); // Future: GpuRenderer renderer(W, H);
+
+    // --- RENDER LOOP ---
+    printf("Rendering %d frames...\n", FRAMES);
+    
+    for (int i = 0; i < FRAMES; ++i) {
+        float time = (float)i / (float)FPS; // Correct time calculation based on FPS
+
+        // 1. Get writable buffer from video encoder
+        int stride;
+        uint8_t* pixels = video.get_pixel_buffer(stride);
+
+        // 2. Ask Renderer to fill it
+        renderer.renderFrame(pixels, stride, time);
+
+        // 3. Send back to video encoder
+        video.submit_frame();
+    }
+
     return 0;
 }
---
+++ renderer_cpu.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "glsl_core.h"
+#include <omp.h>
+#include <algorithm> // for std::max/min
+
+// Logic to link shader dynamically or statically
+#ifndef LINK_SHADER
+    #include "shader.cpp"
+#else
+    void mainImage(vec4 &fragColor, vec2 fragCoord, vec2 iResolution, float iTime);
+#endif
+
+class CpuRenderer {
+    int width, height;
+
+public:
+    CpuRenderer(int w, int h) : width(w), height(h) {
+        #ifdef _OPENMP
+            printf("CPU Renderer: OpenMP Active (%d threads)\n", omp_get_max_threads());
+        #else
+            printf("CPU Renderer: Single Threaded\n");
+        #endif
+    }
+
+    // The function that does the heavy lifting
+    void renderFrame(uint8_t* pixelBuffer, int stride, float time) {
+        vec2 iResolution((float)width, (float)height);
+
+        #pragma omp parallel for
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                // 1. Calculate Pixel
+                vec4 color;
+                vec2 fragCoord((float)x, (float)y);
+                mainImage(color, fragCoord, iResolution, time);
+
+                // 2. Clamp & Convert
+                // We inline the clamp logic here for speed
+                float r = fmaxf(0.0f, fminf(color.x, 1.0f));
+                float g = fmaxf(0.0f, fminf(color.y, 1.0f));
+                float b = fmaxf(0.0f, fminf(color.z, 1.0f));
+
+                // 3. Write to memory
+                int idx = y * stride + x * 3;
+                pixelBuffer[idx + 0] = (uint8_t)(r * 255.0f);
+                pixelBuffer[idx + 1] = (uint8_t)(g * 255.0f);
+                pixelBuffer[idx + 2] = (uint8_t)(b * 255.0f);
+            }
+        }
+    }
+};
